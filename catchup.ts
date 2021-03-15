ed4f793f6aada352dd41f9c350f396d88ae11ea8
Darren Royle <darren@ventureharbour.com>
2021-03-15 15:38:52 +0000

    feat(catchup): you can now limit the catchup by date

--- catchup.ts
+++ catchup.ts
@@ -1,8 +1,8 @@
-import Options from "./options.ts";
+import Options, {HistoryOptions} from "./options.ts";
 import loader from "./loader.ts";
 import {info, SETSILENT} from "./logger.ts";
-import {load} from "./config.ts";
-import {pull, timestamp} from "./git.ts";
+import config, {load, save} from "./config.ts";
+import {checkout, checkoutReset, history, pull, timestamp} from "./git.ts";
 import {join} from "https://deno.land/std/path/mod.ts";
 import {exists} from "https://deno.land/std/fs/mod.ts";
 import { colors } from "https://deno.land/x/cliffy/ansi/colors.ts";
@@ -29,26 +29,31 @@ ${renderMarkdown(report.update).split('\n').map((line) => `   ${line}`).join('\n
   );
 }
 
-export default async function catchup(options: Options): Promise<void> {
+export default async function catchup(options: HistoryOptions): Promise<void> {
   SETSILENT(true);
   loader.show = true;
   loader.text = 'Loading config';
   const reports: Report[] = [];
+  const config = await load(options.config);
   try {
-    const config = await load(options.config);
     const list = Object.keys(config.friends);
     await list.reduce((prev, friend) => {
       return prev.then(async() => {
         loader.text = `Loading ${friend}`;
         const repo = join(config.paths.repos.friends, config.friends[friend].slug);
         await pull(repo);
+        const rows = (await history(repo)).filter(record => dayjs().diff(dayjs(record.timestamp * 1000), 'd') < options.days);
         const plan = join(repo, '.plan');
-        const planExists = await exists(plan)
-        if(planExists) {
-          const lastUpdate = await timestamp(repo);
-          const text = await Deno.readTextFile(plan)
-          reports.push({friend, update: text, updatedAt: new Date(lastUpdate * 1000)});
+        for(var i = 0; i < rows.length; i++) {
+          await checkout(repo, rows[i].sha);
+          const planExists = await exists(plan)
+          if(planExists) {
+            const text = await Deno.readTextFile(plan)
+            reports.push({friend, update: text, updatedAt: new Date(rows[i].timestamp * 1000)});
+          }
+        await checkoutReset(repo);
         }
+        await checkout(repo, 'HEAD');
       })
     }, Promise.resolve())
   } catch(error) {
@@ -57,9 +62,22 @@ export default async function catchup(options: Options): Promise<void> {
     loader.show = false;
     SETSILENT(false);
     if(reports.length) {
-      reports.forEach(printReport);
+      let lineShown = config.lastcheck == undefined;
+      reports
+        .sort((a, b) => a.updatedAt.getTime() - b.updatedAt.getTime())
+        .forEach((report) => {
+          if(!lineShown && config.lastcheck) {
+            if(report.updatedAt > config.lastcheck) {
+              console.log(' ',colors.bgWhite.black('* Your last catchup! *'), '\n')
+              lineShown = true;
+            }
+          }
+          printReport(report);
+        });
     } else {
       info('No posts from your friends, maybe you need more?')
     }
+    config.lastcheck = new Date();
+    await save(options.config, config);
   }
 }
